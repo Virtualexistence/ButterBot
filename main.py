19735d0b960e632942c3703611cbbce93f56a7d3
Virtualexistence <manideepghanta@gmail.com>
2024-04-28 00:04:29 +0530

    Integrating file uploads

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing import Final
 import os
 from dotenv import load_dotenv
-from discord import Intents, Client, Message
+from discord import Intents, Client, Message, File, Embed
 from responses import get_response
 
 load_dotenv()
@@ -21,7 +21,16 @@ async def send_message(message: Message, user_message: str) -> None:
 
     try:
         response: str = get_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
+        if response[1]:
+            if response[2] == 1:
+                await message.channel.send(content=response[0], embed=Embed(type='rich',description="Placeholder", url=response[1]))
+            elif response[2] == 2:
+                await message.channel.send(content=response[0], file=File(response[1]))
+            else:
+                await message.channel.send(file=File('assets/images/butter.png'))
+        else:
+            response = response[0]
+            await message.author.send(response) if is_private else await message.channel.send(response)
 
     except Exception as e:
         print(e)
@@ -41,6 +50,7 @@ async def on_message(message: Message) -> None:
 
     print(f'[{channel}] {username}: {user_message}')
     await send_message(message, user_message)
+
 
 def main() -> None:
     client.run(token=TOKEN)
--- responses.py
+++ responses.py
@@ -3,14 +3,17 @@ from random import choice, randint
 def get_response(user_input: str) -> str:
     lowered: str = user_input.lower()
     yt_link = 'https://www.youtube.com/watch?v=UYJDKSah-Ww'
+    butter_link = 'assets/images/butter.png'
 
     if lowered == '':
         return "Well you're silent"
     elif 'hello' in lowered:
-        return "Hello!"
+        return "Hello!", 0, 0
     elif 'bye' in lowered:
-        return "See ya!"
+        return "See ya!", 0, 0
     elif "tutorial" in lowered:
-        return f"Here's YT link to get started! {yt_link}"
+        return f"Here's YT link to get started: {yt_link}", 0, 0
+    elif 'pass the butter':
+        return "Here's your butter: ", butter_link, 2
     else:
-        return "I don't understand what you're saying"
+        return "I don't understand what you're saying", 0, 0
